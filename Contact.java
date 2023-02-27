786e69c58f1b2afa1f72b453023f525621da106a
Almir Figueiredo <joseamfj@outlook.com>
2023-02-27 12:23:55 -0300

    Create getters and setters

--- Contact.java
+++ Contact.java
@@ -4,5 +4,37 @@ public class Contact {
     private String address;
     private String phone;
 
+    //Constructors:
+    public Contact(){};
     
+
+
+
+
+    //Getters:
+    public String getAddress() {
+        return address;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    //Setters:
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
